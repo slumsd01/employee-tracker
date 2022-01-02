3c4f73b28f6a4398c946813b601866cc2d999d85
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-01-02 11:29:32 -0500

    update employee role

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ function mainMenu() {
             'View all employees',
             'Add a department',
             'Add a role',
-            'Add an employee'
+            'Add an employee',
+            'Update employee role',
+            'Quit'
         ]
     }).then( (selection) => {
         switch (selection.main) {
@@ -43,6 +45,11 @@ function mainMenu() {
             case 'Add an employee':
                 addEmployee()
                 break;
+            case 'Update employee role':
+                updateEmployee()
+                break;
+            case 'Quit':
+                connection.end()
         }
     })
 }
@@ -99,69 +106,111 @@ function addDepartment() {
 
 function addRole() {
     // show departments to choose from
-    connection.promise().query(`SELECT * FROM department`)
-    .then( ([results]) => {
-        console.table(results);
-        inquirer.prompt([
-            {
-                name: 'title',
-                type: 'input',
-                message: 'Enter the title of the new role.'
-            }, 
-            {
-                name: 'salary',
-                type: 'input',
-                message: 'Enter the salary of the new role.'
-            }, 
-            {
-                name: 'department',
-                type: 'input',
-                message: 'What department is this role associated with? (Enter the id number from the table above.)'
+    
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Enter the title of the new role.'
+        }, 
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter the salary of the new role.'
+        }, 
+        {
+            name: 'department',
+            type: 'input',
+            message: function () {
+                connection.promise().query(`SELECT * FROM department`)
+                .then( ([results]) => {
+                    console.log('\n')
+                    console.table(results);
+                    console.log('What department is this role associated with? (Enter the id number from the table above.)')
+                })
             }
-        ]).then( (newRole) => {
-            let params = [ newRole.title, newRole.salary, newRole.department ]
-            connection.promise().query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, params)
-            .then( ([results]) => {
-                console.log(`New role added!`);
-                mainMenu()
-            })
+        }
+    ]).then( (newRole) => {
+        let params = [ newRole.title, newRole.salary, newRole.department ]
+        connection.promise().query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, params)
+        .then( ([results]) => {
+            console.log(`New role added!`);
+            mainMenu()
+        })
+    })
+    
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: "Enter the new employee's first name."
+        }, 
+        {
+            name: 'lastName',
+            type: 'input',
+            message: "Enter the new employee's last name."
+        }, 
+        {
+            name: 'role',
+            type: 'input',
+            message: function () {
+                connection.promise().query(`SELECT role.id, role.title FROM role`)
+                .then( ([results]) => {
+                    console.log('\n')
+                    console.table(results);
+                    console.log('What role does this employee have? (Enter the id number from the table above.)')
+                })
+            }
+        }
+    ]).then( (newEmployee) => {
+        let params = [ newEmployee.firstName, newEmployee.lastName, newEmployee.role ]
+        connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)`, params)
+        .then( ([results]) => {
+            console.log(`New role added!`);
+            mainMenu()
         })
     })
 }
 
-function addEmployee() {
-    // show roles to choose from
-    connection.promise().query
-        (
-            `SELECT role.id, role.title, department.department, role.salary FROM role 
-            JOIN department ON department_id = department.id
-            ORDER BY role.id;`
-        )
-    .then( ([results]) => {
-        console.table(results);
-        inquirer.prompt([
-            {
-                name: 'firstName',
-                type: 'input',
-                message: "Enter the new employee's first name."
-            }, 
-            {
-                name: 'lastName',
-                type: 'input',
-                message: "Enter the new employee's last name."
-            }, 
-            {
-                name: 'role',
-                type: 'input',
-                message: 'What role does this employee have? (Enter the id number from the table above.)'
+function updateEmployee() {
+    inquirer.prompt([
+        {
+            name: 'id',
+            type: 'input',
+            message: function () {
+                connection.promise().query
+                    (
+                        `SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee
+                        JOIN role ON role_id = role.id`
+                    )
+                .then( ([results]) => {
+                    console.log('\n')
+                    console.table(results);
+                    console.log('What employee would you like to update? (Enter the id number from the table above.)')
+                })
             }
-        ]).then( (newEmployee) => {
-            let params = [ newEmployee.firstName, newEmployee.lastName, newEmployee.role ]
-            connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)`, params)
-            .then( ([results]) => {
-                console.log(`New role added!`);
-                mainMenu()
-            })
+        }, 
+        {
+            name: 'role',
+            type: 'input',
+            message: function () {
+                connection.promise().query(`SELECT role.id, role.title FROM role`)
+                .then( ([results]) => {
+                    console.log('\n')
+                    console.table(results);
+                    console.log('What is their updated role? (Enter the id number from the table above.)')
+                })
+            }
+        }
+    ]).then((updatedEmployee) => {
+        let params = [ updatedEmployee.role, updatedEmployee.id ]
+        connection.promise().query(`UPDATE employee SET role_id = (?) WHERE id = (?)`, params)
+        .then( ([results]) => {
+            console.log(`Employee updated!`);
+            mainMenu()
         })
     })
 }
