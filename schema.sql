e1024fa0dd34c2625bdf944cc9bb9bcf6b94ffe6
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-01-02 10:22:15 -0500

    setup to view tables

--- schema.sql
+++ schema.sql
@@ -20,6 +20,5 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INTEGER,
-  manager_id INTEGER,
   FOREIGN KEY (role_id) REFERENCES role(id)
 );
--- server.js
+++ server.js
@@ -10,34 +10,66 @@ const connection = mysql.createConnection({
     database: 'tracker_db'
 })
 
-inquirer
-.prompt({
-    name: 'main',
-    type: 'list',
-    message: 'What would you like to do?',
-    choices: [
-        'View all departments',
-        'View all roles',
-        'View all employees'
-    ]
-})
-.then((selection) => {
-    switch (selection.main) {
-        case 'View all departments':
-            console.log('Departments')
-            break;
-        case 'View all roles':
-            console.log('Roles')
-            break;
-        case 'View all employees':
-            console.log('Employees')
-            break;
-    }
-})
+function mainMenu() {
+    inquirer
+    .prompt({
+        name: 'main',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees'
+        ]
+    })
+    .then((selection) => {
+        switch (selection.main) {
+            case 'View all departments':
+                viewDepartments()
+                break;
+            case 'View all roles':
+                viewRoles()
+                break;
+            case 'View all employees':
+                viewEmployees()
+                break;
+        }
+    })
+}
 
-// connection.promise().query("SELECT * FROM employee")
-//   .then( ([results]) => {
-//     console.table(results);
-//   })
-//   .catch(console.log)
-//   .then( () => connection.end());
+function viewDepartments() {
+    connection.promise().query(`SELECT * FROM department`)
+    .then( ([results]) => {
+        console.table(results);
+        mainMenu()
+    })
+}
+
+function viewRoles() {
+    connection.promise().query
+        (
+            `SELECT role.id, role.title, department.department, role.salary FROM role 
+            JOIN department ON department_id = department.id
+            ORDER BY role.id;`
+        )
+    .then( ([results]) => {
+        console.table(results);
+        mainMenu()
+    })
+}
+
+function viewEmployees() {
+    connection.promise().query
+        (
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department, role.salary FROM employee
+            JOIN role ON employee.role_id = role.id
+            JOIN department ON role.department_id = department.id
+            ORDER BY employee.id;`
+        )
+    .then( ([results]) => {
+        console.table(results);
+        mainMenu()
+    })
+}
+
+mainMenu()
